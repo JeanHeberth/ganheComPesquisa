c870b6290a85dea962dae5bf7faca11cb9c270ff
Jean Heberth Souza Vieira <jeanheberth19@gmail.com>
2023-10-09 15:54:34 -0300

    aaaaaaa

--- src/main/java/br/com/ganheCom/core/DriveFactory.java
+++ src/main/java/br/com/ganheCom/core/DriveFactory.java
@@ -1,6 +1,7 @@
 package br.com.ganheCom.core;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
